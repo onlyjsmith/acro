6839a4c433ad3e7272013ba62689657ede836057
Jonathan Smith <jonathan@impacti.org>
2011-09-03 11:58:25 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Acro::Application.config.secret_token = '501156ce31b464da8bc3bb40f3a5ff620c9f0bdb65e9f7fba2032ff18597d554b65eebd898435264166ff8bb1df10a68aacc10909388b6e0bb15c23fabe55544'
