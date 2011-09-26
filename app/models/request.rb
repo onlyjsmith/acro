64cf6c6025472cbba4f53d9083ee11382e4de0a3
Jonathan Smith <jonathan@impacti.org>
2011-09-26 23:17:01 +0100

    lots of changes - to model, but not tested yet

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,33 +1,32 @@
 class Request < ActiveRecord::Base
+  attr_accessor :words, :acronyms
 
   def search_for_acronyms
-    words = split_words(text)
-    acronyms = find_acronyms(words)
-    # known_acronyms = find_known_acronyms(unique_acronyms)
-    # find_definitions(known_acronyms
-    unique_acronyms = filter_unique(acronyms)
-    search_definitions(unique_acronyms)
+    split_words
+    find_acronyms
+    filter_unique# = filter_unique(find_acronyms)
+    search_definitions
   end
 
-  def split_words(text)
-    text.split(/[^a-zA-Z]/).each{|t| t.gsub(/\W/,'NOTHING')}
+  def split_words
+    @words = text.split(/[^a-zA-Z]/).each{|t| t.gsub(/\W/,'NOTHING')}
   end
 
-  def find_acronyms(words)
-    acronyms = []
-    words.each{|word| acronyms << word if is_acronym?(word)}#word == word.upcase}
+  def find_acronyms
+    @acronyms = []
+    @words.each{|word| @acronyms << word if is_acronym?(word)}#word == word.upcase}
     # TODO: Find a better matcher for acronyms: e.g. including B.B.C. and so on, and ignoring A at start of word?  include hyphens, etc, also lower-case letters
     # TODO: also a way of handling words which might be acronyms, but are uncertain 
     # TODO: check that it's not including duplicates, and if not why not - there's nothing excluding them yet!
-    acronyms
+    @acronyms
   end
 
   def is_acronym?(word)
     word == word.upcase && word.size > 1 ? true : false
   end
 
-  def filter_unique(acronyms)
-    acronyms.uniq!
+  def filter_unique
+    @acronyms = acronyms.uniq
   end
   # def search_definitions(unique_acronyms)
   #   unique_acronyms.each do |unique|
@@ -35,11 +34,11 @@ class Request < ActiveRecord::Base
   #   end
   # end   
     
-  def search_definitions(unique_acronyms) 
-    definitions = []  # TODO: This must return an array of objects, so that EDIT link from results_show page works
+  def search_definitions 
+    definitions = []  # TODO: This must return an array of AR objects not just array, so that EDIT link from results_show page works
     # debugger
-    unique_acronyms.sort!
-    unique_acronyms.each do |a|
+    @acronyms.sort!
+    @acronyms.each do |a|
       found_acronym = Acronym.find_by_abbreviation(a)
       # TODO: Add this adding new unknown acronym back in
       if found_acronym.nil?
